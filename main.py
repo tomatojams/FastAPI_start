0c74e574dd602c285f97ff4b01a83383e1b36259
tomatojams <tomatojams@naver.com>
2023-09-09 13:34:16 +0900

    n

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 from fastapi.responses import FileResponse
 
+
 @app.get("/")
 async def root():
-    return FileResponse('index.html')
+    return FileResponse("index.html")
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 from pydantic import BaseModel
+
 
 class UsrData(BaseModel):
     name: str
-    phone :str
+    phone: str
+
 
 @app.get("/send")
-async def send(data:UsrData):
+async def send(data: UsrData):
     print(data)
-    return  '전송완료'
+    return "전송완료"
