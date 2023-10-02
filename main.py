bf0f20d16ebe68eacbee95c258953ca274b4a5e0
tomatojams <tomatojams@naver.com>
2023-10-02 15:12:34 +0900

    n

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 joy = FastAPI()
 
+origins = ["http://localhost:5173"]
 
+joy.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+  
 @joy.get("/")
 async def root():
     return FileResponse("index.html")
+
+
+@joy.get("/hello")
+async def hello():
+    return {"message": "안녕하세요 파이보"}
 
 
 @joy.get("/hello/{name}")
